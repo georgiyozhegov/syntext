1a3ae77b6b731259c24aee456dfa949caac4c8d3
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2024-10-04 22:46:45 +0400

    Add error-handling in process.py

--- process.py
+++ process.py
@@ -6,23 +6,25 @@ def main():
             data = json.loads(line)
             text = data["text"]
 
-            _, text = text.split("# Тема")
-            topic, text = text.split("# Вопросы")
-            questions, text = text.split("# Текст")
-            
-            topic = topic.strip()
-            questions = questions.strip().split("\n")
-            text = text.strip()
+            try:
+                _, text = text.split("# Тема")
+                topic, text = text.split("# Вопросы")
+                questions, text = text.split("# Текст")
+                
+                topic = topic.strip()
+                questions = questions.strip().split("\n")
+                text = text.strip()
+                questions = list(map(lambda q: q.split(" ", 1)[1], questions))
 
-            questions = list(map(lambda q: q.split(" ", 1)[1], questions))
-
-            data = {
-                    "topic": topic,
-                    "questions": questions,
-                    "answer": text,
-            }
-            data = json.dumps(data, ensure_ascii=False) + "\n"
-            print(data, end="")
+                data = {
+                        "topic": topic,
+                        "questions": questions,
+                        "article": text,
+                }
+                data = json.dumps(data, ensure_ascii=False) + "\n"
+                print(data, end="")
+            except:
+                ...
 
 if __name__ == "__main__":
     main()
